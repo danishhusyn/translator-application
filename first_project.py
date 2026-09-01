9dcb3dfae7870f252eb494b5e7279fe6ea409cbd
Danish Hussain <danish.bsse@gmail.com>
2026-09-01 19:42:49 +0500

    Add files via upload

---
+++ first_project.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from groq import Groq
+
+client=Groq(api_key="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")
+
+st.title("English to urdu translator application")
+
+text=st.text_area("Enter your Text")
+
+if st.button("Translate"):
+    completion=client.chat.completions.create(
+            model="openai/gpt-oss-120b",
+            messages=[
+                {
+                    "role":"user",
+                    "content" : f"act as a translator and your job is only to traslate this text into urdu : \n{text}" 
+                }
+            ]
+
+    )
+
+    result=completion.choices[0].message.content
+
+    st.subheader("Translated text")
+    st.success(result)
+
+
+
+
+
